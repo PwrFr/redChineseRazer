69c72a6f3288b3f8e9870c1b0a14d14de8084efb
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-10-17 18:42:59 +0700

    Menu Bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <MenuBar />
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MenuBar from './components/MenuBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MenuBar
   }
 }
 </script>
