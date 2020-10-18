db593b66b189759dcae8301e93f0a6238cc8a7b4
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-10-18 16:28:07 +0700

    MenuBar WhatsNew(done)

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -4,30 +4,43 @@
   <div class="collapse navbar-collapse container p-0" id="navbarNavAltMarkup">
     <div class="navbar-nav ">
       <a href="" class="navbar-brand mt-1"><img class="" src='src\assets\Menu\razer.png'  width="90" height="13"/> </a>
-      <!-- <a class="nav-item nav-link active mr-1" href="#"><p class="fc pt-2 pl-2 pr-3">WHATS NEW</p></a> -->
-      <div class="dropdown">
+      <div class="dropdown fc">
      <a class="dropbtn nav-item nav-link active mr-1" href="#"><p class="fc pt-2 pl-2 pr-3">WHATS NEW</p>
-      <i class="fa fa-caret-down"></i>
     </a>
-    <div class="dropdown-content mt-1 p-0"> 
-      <div class="row1">
-        <div class="column">
-          <h3>Category 1</h3>
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+    <div class="dropdown-content mt-1"> 
+      <div class="row bg-dropdown w-75 position-fixed m-0 p-2 pt-3 pb-4 text-left">
+        <div class="col-3">
+          <p class="font-weight-bold m-0 pb-1">NEW LAUNCHES</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Arctech Pro for iPhone 12</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer BlackWidow V3</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer DeathAdder V2 Pro</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer BlackWidow V3 Pro</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer BlackShark V2 Pro</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Quartz 2020 Collection</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Mercury 2020 Collection</a>
+
         </div>
-        <div class="column">
-          <h3>Category 2</h3>
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+        <div class="col-2">
+          <p class="font-weight-bold m-0">FEATURED</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Exclusives</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Work From Home</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Back to School</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Campaigns</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer TipStream</a>
+          
         </div>
-        <div class="column">
-          <h3>Category 3</h3>
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+        <div class="col-7">
+          <p class="font-weight-bold m-0">RAZERCON</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Home</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Iskur</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Kraken BT Kitty Edition</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Seiren Mini</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Blade Stealth 13</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Tomahawk</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Viper Ultimate Cyberpunk 2077</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Sneki Snek Plushie</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Sneki Snek X Conservation International</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Go Green with Razer</a>
         </div>
       </div>
     </div>
@@ -57,70 +70,7 @@
 .fc:hover{
   color: white;
 }
-.dropdown .dropbtn {
-  font-size: 16px;  
-  border: none;
-  outline: none;
-  color: #999999;
-  background-color: inherit;
-  font: inherit;
-  margin: 0;
-}
 
-
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  width: 70vw;
-  left: 0;
-  z-index: 1;
-}
-
-.dropdown-content{
-  padding: 16px;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-/* Create three equal columns that floats next to each other */
-.column {
-  float: left;
-  width: 20%;
-  padding: 10px;
-  background-color: #ccc;
-  height: 60vh;
-}
-
-.column a {
-  float: none;
-  color: black;
-  padding: 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-}
-
-.column a:hover {
-  background-color: #ddd;
-}
-
-/* Clear floats after the columns */
-.row1:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
-@media screen and (max-width: 600px) {
-  .column {
-    width: 100%;
-    height: auto;
-  }
-}
 </style>
 <script>
 export default {
