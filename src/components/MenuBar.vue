dd43b56861aca9da03c6ed9f08832a0ebd6e04d9
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-10-18 17:38:41 +0700

    MenuBar(Almost Finish)

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -45,10 +45,204 @@
       </div>
     </div>
   </div> 
-      <a class="nav-item nav-link" href="#"><p class="fc pt-2 pr-3">PRODUCTS</p></a>
-      <a class="nav-item nav-link" href="#"><p class="fc pt-2 pr-3 ml-1">APPS & DOWNLOADS</p></a>
-      <a class="nav-item nav-link" href="#"><p class="fc pt-2 pr-3 ml-1">EXPLORE</p></a>
-      <a class="nav-item nav-link" href="#"><p class="fc pt-2 pr-3 ml-1">SUPPORT</p></a>
+
+  <div class="dropdown fc">
+     <a class="dropbtn nav-item nav-link active mr-1" href="#"><p class="fc  pt-2 pr-3">PRODUCTS</p>
+    </a>
+    <div class="dropdown-content mt-1"> 
+      <div class="row bg-dropdown w-75 position-fixed m-0 p-2 pt-3 pb-4 text-left">
+        <div class="col-2">
+          <p class="font-weight-bold m-0 pb-1">PC</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Laptops</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">eGPUs</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Cases</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Monitors</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">PC Accessories</a>
+          
+
+        </div>
+        <div class="col-2">
+          <p class="font-weight-bold m-0">PERIPHERALS</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Mice</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Mats</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Keyboards</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Audio</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Productivity</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Console</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Streaming</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Peripheral Accessories</a>
+          
+        </div>
+        <div class="col-2">
+          <p class="font-weight-bold m-0">MOBILE</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Controllers</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Audio</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Cases</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Mobile Accessories</a>
+          
+        </div>
+        <div class="col-2">
+          <p class="font-weight-bold m-0">LIFESTYLE</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Gaming Chairs</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">RESPAWN</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Gear & Apparel</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Surgical Masks</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Sneki Snek</a>
+
+          
+        </div>
+        <div class="col-2">
+          <p class="font-weight-bold m-0">CUSTOMS & COLLABS</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Customs</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Cyberpunk 2077</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Gears of War</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Overwatch: Lucio</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Star Wars: Stormtrooper</a>
+
+          
+        </div>
+        <div class="col-2">
+          <p class="font-weight-bold m-0">PAY & REWARDS</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Gold & Silver</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Gold PINs</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Gift Card</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer TipStream</a>
+          
+        </div>
+      </div>
+    </div>
+  </div> 
+
+
+  <div class="dropdown fc">
+     <a class="dropbtn nav-item nav-link active mr-1" href="#"><p class="fc pt-2 pr-3 ">APPS & DOWNLOADS</p>
+    </a>
+    <div class="dropdown-content mt-1"> 
+      <div class="row bg-dropdown w-75 position-fixed m-0 p-2 pt-3 pb-4 text-left">
+        <div class="col-2">
+          <p class="font-weight-bold m-0 pb-1">DRIVERS</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Synapse 3</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Synapse 2 - Legacy</a>
+          
+
+        </div>
+        <div class="col-3 ">
+          <p class="font-weight-bold m-0">DESKTOP</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">THX Spatial Audio</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">7.1 Surround Sound</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Cortex - Game Booster</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item " href="#">Streamer Companion App</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Game Deals</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Chroma RGB</a>
+
+          
+        </div>
+        <div class="col-3">
+          <p class="font-weight-bold m-0">MOBILE</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Cortex Games for Android</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Android Apps</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer iOS Apps</a>
+          
+        </div>
+        <div class="col-2">
+          <p class="font-weight-bold m-0">MEDIA</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Wallpapers</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Chroma Workshop</a>
+          
+        </div>
+      </div>
+    </div>
+  </div> 
+
+
+  <div class="dropdown fc">
+     <a class="dropbtn nav-item nav-link active mr-1" href="#"><p class="fc pt-2 pr-3  ">EXPLORE</p>
+    </a>
+    <div class="dropdown-content mt-1"> 
+      <div class="row bg-dropdown w-75 position-fixed m-0 p-2 pt-3 pb-4 text-left">
+        <div class="col-2">
+          <p class="font-weight-bold m-0 pb-1">ABOUT</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Our Story</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">RazerStores</a>
+          
+
+        </div>
+        <div class="col-2 ">
+          <p class="font-weight-bold m-0">OUR COMMUNITY</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Get Started</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Insider Forums</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Developer Portal</a>
+          
+
+          
+        </div>
+        <div class="col-2">
+          <p class="font-weight-bold m-0">ESPORTS</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Team Razer</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">This Is Esports</a>
+          
+        </div>
+        <div class="col-2">
+          <p class="font-weight-bold m-0">TECHNOLOGY</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Science Behind the Seat</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">HyperClear Mic</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">TriForce Drivers</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">5G Sensor</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Focus+ Sensor</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Hypersense</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">HyperSpeed</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Mechanical Switches</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Optical Switches</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Chroma RGB</a>
+          
+        </div>
+        <div class="col-3">
+          <p class="font-weight-bold m-0">CONCEPTS</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Ultimate Racing Simulator</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Tomahawk Elite</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Project Linda</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Project Valerie</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Project Ariana</a>
+      </div>
+      </div>
+    </div>
+  </div> 
+
+  <div class="dropdown fc">
+     <a class="dropbtn nav-item nav-link active mr-1" href="#"><p class="fc pt-2 pr-3  ">SUPPORT</p>
+    </a>
+    <div class="dropdown-content mt-1"> 
+      <div class="row bg-dropdown w-75 position-fixed m-0 p-2 pt-3 pb-4 text-left">
+        <div class="col-2">
+          <p class="font-weight-bold m-0 pb-1">GET HELP</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Product Support</a>
+          
+
+        </div>
+        <div class="col-3 ">
+          <p class="font-weight-bold m-0">REGISTRATION & WARRANTY</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Warranty FAQ</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Product Registration</a>
+          
+
+          
+        </div>
+        <div class="col-2">
+          <p class="font-weight-bold m-0">ONLINE STORE</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Shipping Guide</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Returns & Refunds</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">Razer Advantage</a>
+
+          
+        </div>
+        <div class="col-3">
+          <p class="font-weight-bold m-0">RAZERCARE</p>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">RazerCare Protection Plan</a>
+          <a class="pt-1 pb-1 d-block text-decoration-none col-item" href="#">RazerCare Store</a>
+         
+          
+        </div>
+        
       
     </div>
     
