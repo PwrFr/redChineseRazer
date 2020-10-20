1989e2905909109809bd41496096c39099224551
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-10-20 15:32:58 +0700

    Revert "Merge branch 'master' of https://github.com/PwrFr/redChineseRazer"
    This reverts commit f6dad22b520a422f2bf3b5651c60d596033d30ab, reversing
    changes made to 93e27fbce2af3771a653d74729a4795699ab47da.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import { createApp } from 'vue'
-import ProductSlot from './components/ProductSlot.vue'
 import App from './App.vue'
 import './index.css'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ProductSlot from './components/ProductSlot.vue'
 createApp(ProductSlot).mount('#productSlot')
 
 createApp(App).mount('#app')
-createApp(ProductSlot).mount('#productSlot')
-
