93e27fbce2af3771a653d74729a4795699ab47da
Artermis <nopphadon.phanwong@gmail.com>
2020-10-20 09:43:24 +0700

    Noting Update

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import ProductSlot from './components/ProductSlot.vue'
+createApp(ProductSlot).mount('#productSlot')
 
 createApp(App).mount('#app')
