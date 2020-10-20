7284b4b98dfaeaf68b5e4004e4ae99b5ceaa2473
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-10-20 21:51:34 +0700

    add menubar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import ProductSlot from './components/ProductSlot.vue'
+import MenuBar from './components/MenuBar.vue'
+createApp(MenuBar).mount('#Menu')
 createApp(ProductSlot).mount('#productSlot')
 
 createApp(App).mount('#app')
