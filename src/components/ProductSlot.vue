38a63b2b174be0c448238f73f6169a808b88a860
kaolnwza <kaow_jijuy999@hotmail.com>
2020-11-03 21:37:00 +0700

    update with tab button

--- src/components/ProductSlot.vue
+++ src/components/ProductSlot.vue
@@ -1,21 +1,48 @@
-<script>
-/*
-import Vue from "vue";
-
-import BootstrapVue from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-
-Vue.use(BootstrapVue);*/
-</script>
 
 
-<template>
-  <div class="container-fluid">
-    <div class="row">
-      <img class="col img-full" src="../assets/products/cases/1.jpg" />
 
-      <div class="col-2 right-box" style>
+<template >
+  <div class="container-fluid" style="padding: 0px;">
+    <div class="row p-0 m-0">
+      <div
+        class="col-xl-9 col-lg-12 col-img-full"
+        v-bind:style="{ backgroundImage: 'url(' + imageSrc + ')' }"
+      >
+        <div class="image-tab">
+          <lable>
+            <div class="image-tab-list">
+              <input type="radio" name="test" @click="changeImage(1)" />
+              <img src="../assets/products/cases/bar/1.jpg" class="image-tab-border" />
+            </div>
+          </lable>
+          <lable>
+            <div class="image-tab-list">
+              <input type="radio" name="test" @click="changeImage(2)" />
+              <img src="../assets/products/cases/bar/2.jpg" class="image-tab-border" />
+            </div>
+          </lable>
+          <lable>
+            <div class="image-tab-list">
+              <input type="radio" name="test" @click="changeImage(3)" />
+              <img src="../assets/products/cases/bar/3.jpg" class="image-tab-border" />
+            </div>
+          </lable>
+          <lable>
+            <div class="image-tab-list">
+              <input type="radio" name="test" @click="changeImage(4)" />
+              <img src="../assets/products/cases/bar/4.jpg" class="image-tab-border" />
+            </div>
+          </lable>
+          <lable>
+            <div class="image-tab-list">
+              <input type="radio" name="test" @click="changeImage(5)" />
+              <img src="../assets/products/cases/bar/5.jpg" class="image-tab-border" />
+            </div>
+          </lable>
+        </div>
+      </div>
+      <!-- -->
+      <div class="col-xl-3 col-lg-12 right-box w-100">
         <p class="header">Razer Tomahawk Mini-ITX</p>
         <p class="head-description">Mini-ITX Gaming Chassis with Razer Chroma RGB</p>
         <p class="head-price">US$179.99</p>
@@ -27,33 +54,78 @@ Vue.use(BootstrapVue);*/
           </ul>
         </div>
         <div class="select-session">
-          <select id="cars" name="carlist" form="carform">
+          <select id="test" name="carlist" form="carform">
             <option value="volvo">Volvo</option>
             <option value="saab">Saab</option>
             <option value="opel">Opel</option>
             <option value="audi">Audi</option>
           </select>
         </div>
+        <p class="shipping-date">Ships on November 05, 2020</p>
+        <button class="order-button">Pre-Order</button>
+        <p class="product-status">Also Available At</p>
       </div>
     </div>
   </div>
 </template>
+<script>
+//document.getElementsByClassName("image-tab-list").style.borderColor = "red";
 
+export default {
+  data() {
+    return {
+      imageSrc: "/src/assets/products/cases/1.jpg"
+    };
+  },
+  methods: {
+    changeImage(num) {
+      this.imageSrc = "/src/assets/products/cases/" + num + ".jpg";
+      this.$refs.bar1.style;
+      this.color = "yellow";
+    }
+  }
+};
+</script>
 <style>
+@font-face {
+  font-family: RazerF5-Reg;
+  src: url("../assets/font/RazerF5-Reg.otf");
+}
 body {
   background-color: #000;
 }
-.slotBox {
+.row {
+  height: 100vh;
+}
+.col-img-full {
+  background-size: 100%;
+  background-repeat: no-repeat;
+}
+.image-tab {
+  display: flex;
+  flex-direction: column;
+  left: 2.9375rem;
+  position: absolute;
+  top: 2.5rem;
 }
 
-.img-full {
-  max-width: 79%;
-  padding: 0px;
+.image-tab-list {
+  height: 5rem;
+  width: 5rem;
+  margin-bottom: 10px;
+}
+
+.image-tab-border {
+  border: 1px solid #999;
+}
+
+.right-box {
+  background-color: black;
 }
 
 .header {
   padding-top: 40px;
-  font-family: RazerF5, helvetica neue, Arial, noto sans, sans-serif;
+  font-family: RazerF5-Reg;
   font-size: 1.3125rem;
   font-weight: 400;
   line-height: 1.1em;
@@ -64,16 +136,16 @@ body {
 }
 
 .head-description {
+  display: block;
+
   margin-top: 0.5rem;
-  font-family: Roboto, helvetica neue, Arial, noto sans, sans-serif,
-    apple color emoji, segoe ui emoji, segoe ui symbol, noto color emoji;
+  font-family: RazerF5-Reg;
   font-size: 0.875rem;
-  line-height: 1.2;
   color: #fff !important;
 }
 
 .head-price {
-  font-family: RazerF5, helvetica neue, Arial, noto sans, sans-serif;
+  font-family: RazerF5-Reg;
   font-weight: 400;
   line-height: 1.5;
   font-size: 1.3125rem;
@@ -87,16 +159,13 @@ body {
   margin-top: 2.1875rem;
   list-style: disc;
   color: #999;
-  font-family: Roboto, helvetica neue, Arial, noto sans, sans-serif,
-    apple color emoji, segoe ui emoji, segoe ui symbol, noto color emoji;
+  font-family: RazerF5-Reg;
   font-weight: 400;
+  margin-bottom: 3.75rem;
 }
 ul {
   text-align: left;
   padding-left: 1rem;
-}
-
-.select-session {
 }
 
 .select-session select {
@@ -106,8 +175,7 @@ ul {
   border-radius: 0.25rem;
   color: #999;
   display: flex;
-  font-family: Roboto, helvetica neue, Arial, noto sans, sans-serif,
-    apple color emoji, segoe ui emoji, segoe ui symbol, noto color emoji;
+  font-family: RazerF5-Reg;
   font-size: 0.875rem;
   min-height: 3rem;
   min-width: var(--dropdown-custom-min-width, 14.0625rem);
@@ -119,7 +187,7 @@ ul {
   background-image: ;
 }
 option {
-  font-family: Roboto, helvetica neue, sans-serif;
+  font-family: RazerF5-Reg;
   background-color: #44d62c;
   font-size: inherit;
   line-height: 3em;
@@ -128,14 +196,74 @@ option {
   height: 2rem !important;
 }
 
+.shipping-date {
+  color: #999;
+  font-size: 0.875rem;
+  margin-bottom: 0;
+  margin-top: 30px;
+}
+
+.order-button {
+  border-radius: 0.1875rem;
+  color: #000;
+  font-family: RazerF5-Reg;
+  font-size: 0.875rem;
+  font-weight: 700;
+  height: 2.5rem;
+  line-height: 1.4em;
+  max-height: none;
+  padding-left: 0.625rem;
+  padding-right: 0.625rem;
+  text-overflow: ellipsis;
+  text-transform: uppercase;
+  white-space: nowrap;
+  background-color: #44d62c;
+  border: 0;
+  margin-top: 0.9375rem;
+  width: 100%;
+  max-width: 19.375rem;
+}
+
+.product-status {
+  background: 0 0 !important;
+  border-radius: 2px !important;
+  color: #44d62c !important;
+  cursor: pointer !important;
+  font: normal normal normal 14px/17px Roboto, sans-serif !important;
+  margin: 5px !important;
+  padding: 8px 12px !important;
+  -webkit-user-select: none !important;
+  -moz-user-select: none !important;
+  -ms-user-select: none !important;
+  user-select: none !important;
+  visibility: visible !important;
+  text-align: center;
+  margin-top: 1rem;
+  max-width: 19.375rem;
+}
+
 p {
   margin: 0px;
   padding: 0px;
 }
-.right-box {
-  background-color: #000;
-  width: 375px;
-  padding-left: 37px;
-  min-width: 375px;
+
+[type="radio"] {
+  position: absolute;
+  opacity: 0;
+  width: 5rem;
+  height: 5rem;
+}
+[type="radio"]:hover + img {
+  border-color: #44d62c;
+  border: 1px solid #44d62c;
+}
+
+[type="radio"] + img {
+  cursor: pointer;
+}
+
+[type="radio"]:checked + img {
+  border-color: #44d62c;
+  border: 1px solid #44d62c;
 }
 </style>
