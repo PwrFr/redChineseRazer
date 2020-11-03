e9f66a698d8f28c4ff32bff048f4ed2ceaa5de1a
Artermis <nopphadon.phanwong@gmail.com>
2020-11-03 10:57:48 +0700

    Edit by kaozaza

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import ProductSlot from './components/ProductSlot.vue'
+
 import MenuBar from './components/MenuBar.vue'
 
 createApp(MenuBar).mount('#Menu')
+import ProductSlot from './components/ProductSlot.vue'
 createApp(ProductSlot).mount('#productSlot')
 createApp(App).mount('#app')
