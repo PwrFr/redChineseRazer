243d4885e969d55a2b2dc81fb37bd861efc9df25
Artermis <nopphadon.phanwong@gmail.com>
2020-11-03 23:20:52 +0700

    Add ProductSlot.Vue and fix bug

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <MenuBar  />
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <MenuBar/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+  <ProductSlot/>
+  
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import MenuBar from './components/MenuBar.vue'
+import ProductSlot from './components/ProductSlot.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    MenuBar
+    MenuBar,
+    ProductSlot
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-import MenuBar from './components/MenuBar.vue'
+//import MenuBar from './components/MenuBar.vue'
 
-createApp(MenuBar).mount('#Menu')
-import ProductSlot from './components/ProductSlot.vue'
-createApp(ProductSlot).mount('#productSlot')
+// createApp(MenuBar).mount('#Menu')
+//import ProductSlot from './components/ProductSlot.vue'
+//createApp(ProductSlot).mount('#productSlot')
 createApp(App).mount('#app')
