7f90347171924c5a61e3f3c2fe86812bbc12728c
WSBoos <56477747+WSBoos@users.noreply.github.com>
2020-11-04 01:58:44 +0700

    Update ProductSlot.vue

--- src/components/ProductSlot.vue
+++ src/components/ProductSlot.vue
@@ -53,9 +53,89 @@
             <li>{{description3}}</li>
           </ul>
         </div>
+        
         <!-- boos here --->
+        <!-- onload="myFunction('3','1','2')" onclick="onClickColor()" [เอาไปใส่ที่body]-->
+        <div class="container">
+            <div id="box" class="box">
+              <div  class="select-box">
+                <div  class="options-container">
+      
+                  <div onclick="myFunction('1','2','3')" class="option">
+                    <input type="radio" class="radio" id="Standard - Black" name="category" />
+                    <label  id="1" for="Standard - Black">Standard - Black</label>
+                   </div>
+      
+                  <div onclick="myFunction('2','1','3')" class="option">
+                    <input type="radio" class="radio" id="Standard - Mercury" name="category" />
+                    <label id="2" for="Standard - Mercury">Standard - Mercury</label>
+                  </div>
+      
+                  <div onclick="myFunction('3','1','2')" class="option">
+                    <input type="radio" class="radio" id="Chroma" name="category" />
+                    <label id="3" for="Chroma">Chroma</label>
+                  </div>
+              </div>
+      
+              <div class="selected">
+                  Chroma
+              </div>
+           
+            </div>
+          </div>
+      </div>
+      <div class="box1" id="box1">
+         <b> Model</b>
+      </div>
+      
+          <script>
+                const selected = document.querySelector(".selected");
+                const optionsContainer = document.querySelector(".options-container");
+                const optionsList = document.querySelectorAll(".option");
+                let num = 0;
+                let onum = 0;
 
+                selected.addEventListener("click", () => {
+                optionsContainer.classList.toggle("active");
+                num+=1;
+                });
+
+                optionsList.forEach(o => {
+                    o.addEventListener("click", () => {
+                    selected.innerHTML = o.querySelector("label").innerHTML;
+                    optionsContainer.classList.remove("active");
+                });
+                });
+
+
+                function myFunction(change,not_change1, not_change2)
+                {
+                    document.getElementById(change).setAttribute("style", "color: #f5f5f5;font-size:inherit");
+                    document.getElementById(not_change1).setAttribute("style", "color: 8d8d8d");
+                    document.getElementById(not_change2).setAttribute("style", "color: 8d8d8d");
+                    
+                }
+
+                function onClickColor() {
+                if ((onum===num) || (num % 2 == 0)) {
+                    document.getElementById('box').setAttribute("style", "border: 1px solid #adadad;");
+                    document.getElementById('box1').setAttribute("style", "color: #adadad;");
+                    if (num % 2 != 0){
+                    optionsContainer.classList.remove("active");
+                    num += 1;
+                    }
+                }
+
+                else{
+                    document.getElementById('box').setAttribute("style", "border: 2px solid #43d32b !important;");
+                    document.getElementById('box1').setAttribute("style", "color: #43d32b;");
+                }
+                onum = num;
+                }
+          </script>
+          
         <!-- end --->
+        
         <p class="shipping-date">{{shippingDate}}</p>
         <button class="order-button">{{buyingStatus}}</button>
         <p class="product-status">Also Available At</p>
@@ -65,6 +145,174 @@
 </template>
 
 <style>
+
+<!-- boos here --->
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Roboto", sans-serif;
+  background: black;
+}
+
+.container {
+  margin-top: 100px;
+  /* padding: 32px; */
+  margin-left: 10px;
+}
+
+.select-box {
+  display: flex;
+  /* width: 400px; */
+  flex-direction: column;
+}
+
+.select-box .options-container {
+  background: #2f3640;
+  color: #8d8d8d;
+  max-height: 0;
+  width: 223px;
+  opacity: 0;
+  transition: all 0.4s;
+  border-radius: 8px;
+  overflow: hidden;
+  margin-left: -4px;
+  margin-top: -5px;
+
+  order: 1;
+}
+
+.selected {
+  border-radius: 8px;
+  margin-bottom: 8px;
+  color: #f5f6fa;
+  position: relative;
+  order: 0;
+}
+
+.selected::after {
+  content: "";
+  background: url("img/arrow-down.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+  position: absolute;
+  height: 100%;
+  width: 32px;
+  right: 10px;
+  top: 5px;
+
+  transition: all 0.4s;
+}
+
+.select-box .options-container.active {
+  max-height: 240px;
+  opacity: 1;
+  overflow-y: scroll;
+  border: 1px inset  #1f1e1e;
+  border-radius: 3px;
+}
+
+.select-box .options-container.active + .selected::after {
+  transform: rotateX(180deg);
+  top: -6px;
+}
+
+.select-box .options-container::-webkit-scrollbar {
+  width: 0.1px;
+  background: #111111;
+  border-radius: 0 8px 8px 0;
+}
+
+.select-box .options-container::-webkit-scrollbar-thumb {
+  background: #525861;
+  border-radius: 0 8px 8px 0;
+}
+
+.option{
+  background-color: #111111;
+  border: #111111 1px solid;
+  height: 32px;
+}
+
+.option{
+  background-color: #111111;
+  border: #111111 1px solid;
+  height: 32px;
+  font-size: 13.9px;
+  padding: 8px 12px;
+  cursor: pointer;
+}
+
+.selected {
+  margin-left: -12px;
+  padding: 14px 24px;
+  cursor: pointer;
+  font-size: 13.9px;
+}
+
+.vertical-center {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+
+.select-box .option:hover {
+  background: #414b57;
+}
+
+.select-box label {
+  cursor: pointer;
+}
+
+.select-box .option .radio {
+  display: none;
+}
+
+
+.box {
+  border: 1px solid #999999;
+  border-radius: 4px;
+  height: 48px;
+  width: 225px;
+  margin-left: 15px;
+} 
+
+.box1 {
+  color: #adadad;
+  background: #000;
+  font-size: 13px;
+  margin-bottom: -10px;
+  width: 43px;
+  text-align: center;
+  margin-top: -54px;
+  margin-left: 35px;
+  font-family: Calibri;
+}
+
+select {
+  background-color: black;
+  color: aliceblue;
+  /* border: 1px solid #999999; */
+  /* margin-top: 15px;
+  margin-left: 10px; */
+  border-style: none;
+  margin-top: 10px;
+  margin-left: 2px;
+  height: 35px;
+  width: 220px;
+  border-radius: 4px;
+  z-index: -100;
+}
+
+.box:hover{
+  border-color: white !important;
+}
+<!-- end --->
+
 @font-face {
   font-family: RazerF5-Reg;
   src: url("../assets/font/RazerF5-Reg.otf");
