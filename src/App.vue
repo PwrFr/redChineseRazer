8991fcdd08ae75687b143366c542b8602d3395a5
kaolnwza <kaow_jijuy999@hotmail.com>
2020-11-05 19:51:18 +0700

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
-  <MenuBar  />
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <MenuBar />
   <ImageRow />
+  <ProductSlot
+    imageTab1="/src/assets/products/cases/bar/1.jpg"
+    imageTab2="/src/assets/products/cases/bar/2.jpg"
+    imageTab3="/src/assets/products/cases/bar/3.jpg"
+    imageTab4="/src/assets/products/cases/bar/4.jpg"
+    imageTab5="/src/assets/products/cases/bar/5.jpg"
+    backgroundSrc="/src/assets/products/cases/1.jpg"
+    imageLocateSrc="/src/assets/products/cases/"
+    headerText="Razer Tomahawk Mini-ITX"
+    headerDes="Mini-ITX Gaming Chassis with Razer Chroma RGB"
+    priceText="179.99"
+    description1="Dual-sided tempered glass swivel doors"
+    description2="Powered by Razer Chroma™ RGB"
+    description3="Ventilated top panel and dust filters"
+    shippingDate="Ships on November 05, 2020"
+    buyingStatus="Pre-Order"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import MenuBar from './components/MenuBar.vue'
-import ImgRow from './components/ImageRow.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import MenuBar from "./components/MenuBar.vue";
+import ImgRow from "./components/ImageRow.vue";
+import ProductSlot from "./components/ProductSlot.vue";
 export default {
   name: "App",
   components: {
     HelloWorld,
     MenuBar,
-    ImgRow
+    ImgRow,
+    ProductSlot
   }
 };
 </script>
