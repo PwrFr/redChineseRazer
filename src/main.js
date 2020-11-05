6ccccc42d1799e2f723a7cc9ddee2f104f6e58ee
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-11-05 19:22:14 +0700

    idk

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+<<<<<<< HEAD
 import ProductSlot from './components/ProductSlot.vue'
 import MenuBar from './components/MenuBar.vue'
 import Img from './components/ImageRow.vue'
@@ -8,4 +9,12 @@ import Img from './components/ImageRow.vue'
 createApp(MenuBar).mount('#Menu')
 createApp(Img).mount('#ImgRow')
 createApp(ProductSlot).mount('#productSlot')
+=======
+
+//import MenuBar from './components/MenuBar.vue'
+
+// createApp(MenuBar).mount('#Menu')
+//import ProductSlot from './components/ProductSlot.vue'
+//createApp(ProductSlot).mount('#productSlot')
+>>>>>>> 7f90347171924c5a61e3f3c2fe86812bbc12728c
 createApp(App).mount('#app')
