2347e35cd5fa005dfbc533a090867b3cd31029f3
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-11-05 19:20:13 +0700

    idk

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <MenuBar  />
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <ImageRow />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import MenuBar from './components/MenuBar.vue'
+import ImgRow from './components/ImageRow.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    MenuBar
+    MenuBar,
+    ImgRow
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './index.css'
 import ProductSlot from './components/ProductSlot.vue'
 import MenuBar from './components/MenuBar.vue'
+import Img from './components/ImageRow.vue'
 
 createApp(MenuBar).mount('#Menu')
+createApp(Img).mount('#ImgRow')
 createApp(ProductSlot).mount('#productSlot')
 createApp(App).mount('#app')
