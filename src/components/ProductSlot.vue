099092ce236d7001adbc9a81cd96164ebca60cc3
kaolnwza <kaow_jijuy999@hotmail.com>
2020-11-15 19:06:50 +0700

    update click 1 time selector on

--- src/components/ProductSlot.vue
+++ src/components/ProductSlot.vue
@@ -1,140 +1,132 @@
 <template >
-<div  @click="onClickColor()">
-  <!-- <div  class="container-fluid" style="padding: 0px; background-color: black">
-    <div class="row p-0 mt-0 row-product">
-      <div
-        class="col-xl-9 col-md-12 col-img-full"
-        v-bind:style="{ backgroundImage: 'url(' + bgComponent + ')' }"
-      >
-        <div class="image-tab">
-          <lable>
-            <div class="image-tab-list">
-              <input type="radio" name="test" @click="changeImage(1)" class="rdo_style" checked />
-              <img :src="imageTab1" class="image-tab-border" />
-            </div>
-          </lable>
-          <lable>
-            <div class="image-tab-list">
-              <input type="radio" name="test" @click="changeImage(2)" class="rdo_style" />
-              <img :src="imageTab2" class="image-tab-border" />
-            </div>
-          </lable>
-          <lable>
-            <div class="image-tab-list">
-              <input type="radio" name="test" @click="changeImage(3)" class="rdo_style" />
-              <img :src="imageTab3" class="image-tab-border" />
-            </div>
-          </lable>
-          <lable>
-            <div class="image-tab-list">
-              <div v-if="4 - imageAmount > 0">
-                <img :src="imageTab4" class="image-tab-border" :style="hiddenBorder4" />
+  <div @click="onClickColor()">
+    <div class="container-fluid" style="padding: 0px; background-color: black">
+      <div class="row p-0 mt-0 row-product">
+        <div
+          class="col-xl-9 col-md-12 col-img-full"
+          v-bind:style="{ backgroundImage: 'url(' + bgComponent + ')' }"
+        >
+          <div class="image-tab">
+            <lable>
+              <div class="image-tab-list">
+                <input type="radio" name="test" @click="changeImage(1)" class="rdo_style" checked />
+                <img :src="imageTab1" class="image-tab-border" />
               </div>
-              <div v-else>
-                <input type="radio" name="test" @click="changeImage(4)" class="rdo_style" />
-                <img :src="imageTab4" class="image-tab-border" />
+            </lable>
+            <lable>
+              <div class="image-tab-list">
+                <input type="radio" name="test" @click="changeImage(2)" class="rdo_style" />
+                <img :src="imageTab2" class="image-tab-border" />
               </div>
-            </div>
-          </lable>
-          <lable>
-            <div class="image-tab-list">
-              <div v-if="5 - imageAmount > 0">
-                <img :src="imageTab5" class="image-tab-border" :style="hiddenBorder5" />
+            </lable>
+            <lable>
+              <div class="image-tab-list">
+                <input type="radio" name="test" @click="changeImage(3)" class="rdo_style" />
+                <img :src="imageTab3" class="image-tab-border" />
               </div>
-              <div v-else>
-                <input type="radio" name="test" @click="changeImage(5)" class="rdo_style" />
-                <img :src="imageTab5" class="image-tab-border" />
-              </div>
-            </div>
-          </lable>
-          <lable>
-            <div class="image-tab-list">
-              <div v-if="6 - imageAmount > 0">
-                <img :src="imageTab6" class="image-tab-border" :style="hiddenBorder6" />
-              </div>
-              <div v-else>
-                <input type="radio" name="test" @click="changeImage(6)" class="rdo_style" />
-                <img :src="imageTab6" class="image-tab-border" />
-              </div>
-            </div>
-          </lable>
-        </div>
-      </div> -->
-
-
-      <!--leftside -->
-
-      
-      <!-- <div class="col-xl-3 col-md-12 right-box w-100 pl-4 pt-2 pr-5">
-        <p class="header">{{headerText}}</p>
-        <p class="head-description">{{headerDes}}</p>
-        <p class="head-price">US${{priceText}}</p>
-        <div class="ul-session">
-          <ul>
-            <li>{{description1}}</li>
-            <li v-if="2 <= desAmount">{{description2}}</li>
-            <li v-if="3 <= desAmount">{{description3}}</li>
-            <li v-if="4 <= desAmount">{{description4}}</li>
-            <li v-if="5 <= desAmount">{{description5}}</li>
-            <li v-if="6 <= desAmount">{{description6}}</li>
-            <li v-if="7 <= desAmount">{{description7}}</li>
-            <li v-if="8 <= desAmount">{{description8}}</li>
-            <li v-if="9 <= desAmount">{{description9}}</li>
-            <li v-if="10 <= desAmount">{{description10}}</li>
-            <li v-if="11 <= desAmount">{{description11}}</li>
-          </ul>
-        </div> -->
-
-
-
-      <!-- boos here --->
-        <div   v-if="modelBoxDisappear == 0">
-          <div  class="model-box-div">
-            <div @click="test()" id="box" class="model-box">
-              <div class="select-box">
-                <div class="model-options-container" ref="here">
-                  <div @click="myFunction('1','2','3')" class="box-option">
-                    <input type="radio" class="model-radio" id="Standard - Black" name="category" />
-                    <label id="1" for="Standard - Black">Standard - Black</label>
-                  </div>
-
-                  <div @click="myFunction('2','1','3')" class="box-option">
-                    <input type="radio" class="model-radio" id="Standard - Mercury" name="category" />
-                    <label id="2" for="Standard - Mercury">Standard - Mercury</label>
-                  </div>
-
-                  <div @click="myFunction('3','1','2')" class="box-option">
-                    <input type="radio" class="model-radio" id="Chroma" name="category" />
-                    <label id="3" for="Chroma">Chroma</label>
-                  </div>
+            </lable>
+            <lable>
+              <div class="image-tab-list">
+                <div v-if="4 - imageAmount > 0">
+                  <img :src="imageTab4" class="image-tab-border" :style="hiddenBorder4" />
                 </div>
-
-                <div class="selected">{{modelFirstItem}}</div>
+                <div v-else>
+                  <input type="radio" name="test" @click="changeImage(4)" class="rdo_style" />
+                  <img :src="imageTab4" class="image-tab-border" />
+                </div>
               </div>
-            </div>
-            <div class="model-box-1" id="box1">
-              <b>Model</b>
-            </div>
+            </lable>
+            <lable>
+              <div class="image-tab-list">
+                <div v-if="5 - imageAmount > 0">
+                  <img :src="imageTab5" class="image-tab-border" :style="hiddenBorder5" />
+                </div>
+                <div v-else>
+                  <input type="radio" name="test" @click="changeImage(5)" class="rdo_style" />
+                  <img :src="imageTab5" class="image-tab-border" />
+                </div>
+              </div>
+            </lable>
+            <lable>
+              <div class="image-tab-list">
+                <div v-if="6 - imageAmount > 0">
+                  <img :src="imageTab6" class="image-tab-border" :style="hiddenBorder6" />
+                </div>
+                <div v-else>
+                  <input type="radio" name="test" @click="changeImage(6)" class="rdo_style" />
+                  <img :src="imageTab6" class="image-tab-border" />
+                </div>
+              </div>
+            </lable>
           </div>
         </div>
-        <!-- end --->
 
+        <!--leftside -->
 
+        <div class="col-xl-3 col-md-12 right-box w-100 pl-4 pt-2 pr-5">
+          <p class="header">{{headerText}}</p>
+          <p class="head-description">{{headerDes}}</p>
+          <p class="head-price">US${{priceText}}</p>
+          <div class="ul-session">
+            <ul>
+              <li>{{description1}}</li>
+              <li v-if="2 <= desAmount">{{description2}}</li>
+              <li v-if="3 <= desAmount">{{description3}}</li>
+              <li v-if="4 <= desAmount">{{description4}}</li>
+              <li v-if="5 <= desAmount">{{description5}}</li>
+              <li v-if="6 <= desAmount">{{description6}}</li>
+              <li v-if="7 <= desAmount">{{description7}}</li>
+              <li v-if="8 <= desAmount">{{description8}}</li>
+              <li v-if="9 <= desAmount">{{description9}}</li>
+              <li v-if="10 <= desAmount">{{description10}}</li>
+              <li v-if="11 <= desAmount">{{description11}}</li>
+            </ul>
+          </div>
 
+          <!-- boos here --->
+          <div v-if="modelBoxDisappear == 0">
+            <div class="model-box-div">
+              <div id="box" class="model-box">
+                <div class="select-box">
+                  <div class="model-options-container" ref="here">
+                    <div @click="myFunction('1','2','3')" class="box-option">
+                      <input type="radio" class="model-radio" id="Standard - Black" name="category" />
+                      <label id="1" for="Standard - Black">Standard - Black</label>
+                    </div>
 
+                    <div @click="myFunction('2','1','3')" class="box-option">
+                      <input
+                        type="radio"
+                        class="model-radio"
+                        id="Standard - Mercury"
+                        name="category"
+                      />
+                      <label id="2" for="Standard - Mercury">Standard - Mercury</label>
+                    </div>
 
+                    <div @click="myFunction('3','1','2')" class="box-option">
+                      <input type="radio" class="model-radio" id="Chroma" name="category" />
+                      <label id="3" for="Chroma">Chroma</label>
+                    </div>
+                  </div>
 
-        <!-- <p class="shipping-date">{{shippingDate}}</p>
-        <button class="order-button">{{buyingStatus}}</button>
+                  <div class="selected">{{modelFirstItem}}</div>
+                </div>
+              </div>
+              <div class="model-box-1" id="box1">
+                <b>Model</b>
+              </div>
+            </div>
+          </div>
+          <!-- end --->
 
-        <p class="product-status">{{buyingAt}}</p>
+          <p class="shipping-date">{{shippingDate}}</p>
+          <button class="order-button">{{buyingStatus}}</button>
 
-
-
-        
+          <p class="product-status">{{buyingAt}}</p>
+        </div>
       </div>
     </div>
-  </div> -->
   </div>
 </template>
 
@@ -512,8 +504,8 @@ export default {
   },
   data() {
     return {
-      num : 0,
-      onum : 0,
+      num: 0,
+      onum: 0,
 
       hiddenBorder4: {
         border: "0px"
@@ -533,6 +525,9 @@ export default {
       bgComponent: this.backgroundSrc
     };
   },
+  mounted() {
+    this.test();
+  },
   methods: {
     changeImage(num) {
       if (num <= this.imageAmount) {
@@ -542,70 +537,62 @@ export default {
     },
 
     test() {
-const selected = document.querySelector(".selected");
-const optionsContainer = document.querySelector(".model-options-container");
-const optionsList = document.querySelectorAll(".option");
+      const selected = document.querySelector(".selected");
+      const optionsContainer = document.querySelector(
+        ".model-options-container"
+      );
+      const optionsList = document.querySelectorAll(".option");
 
-selected.addEventListener("click", () => {
-  optionsContainer.classList.toggle("active");
-  this.nnum+=1;
-});
+      selected.addEventListener("click", () => {
+        optionsContainer.classList.toggle("active");
+        this.nnum += 1;
+      });
 
-optionsList.forEach(o => {
-    o.addEventListener("click", () => {
-    selected.innerHTML = o.querySelector("label").innerHTML;
-    optionsContainer.classList.remove("active");
-
-  });
-});
-
+      optionsList.forEach(o => {
+        o.addEventListener("click", () => {
+          selected.innerHTML = o.querySelector("label").innerHTML;
+          optionsContainer.classList.remove("active");
+        });
+      });
     },
 
-   myFunction(change,not_change1, not_change2)
-{
-    document.getElementById(change).setAttribute("style", "color: #f5f5f5;font-size:inherit");
-    document.getElementById(not_change1).setAttribute("style", "color: 8d8d8d");
-    document.getElementById(not_change2).setAttribute("style", "color: 8d8d8d");
-    
-},
+    myFunction(change, not_change1, not_change2) {
+      document
+        .getElementById(change)
+        .setAttribute("style", "color: #f5f5f5;font-size:inherit");
+      document
+        .getElementById(not_change1)
+        .setAttribute("style", "color: 8d8d8d");
+      document
+        .getElementById(not_change2)
+        .setAttribute("style", "color: 8d8d8d");
+    }
 
+    //  onClickColor() {
 
+    // console.log("nomal")
 
-//  onClickColor() {
-   
-   
-// console.log("nomal")
+    //   if ((this.onum === this.nnum) || (this.nnum % 2 == 0)) {
+    //     document.getElementById('box').setAttribute("style", "border: 1px solid #adadad;");
+    //     document.getElementById('box1').setAttribute("style", "color: #adadad;");
+    //     console.log("bigif")
 
-//   if ((this.onum === this.nnum) || (this.nnum % 2 == 0)) {
-//     document.getElementById('box').setAttribute("style", "border: 1px solid #adadad;");
-//     document.getElementById('box1').setAttribute("style", "color: #adadad;");
-//     console.log("bigif")
+    //     if (this.nnum % 2 !== 0){
+    //       optionsContainer.classList.remove("active");
+    //       this.nnum += 1;
 
-//     if (this.nnum % 2 !== 0){
-//       optionsContainer.classList.remove("active");
-//       this.nnum += 1;
-   
-//       console.log("if")
+    //       console.log("if")
 
-//     }
-//   }
+    //     }
+    //   }
 
-//   else{
-//       document.getElementById('box').setAttribute("style", "border: 2px solid #43d32b !important;");
-//       document.getElementById('box1').setAttribute("style", "color: #43d32b;");
-//       console.log("el")
-//   }
-//   this.onum = this.nnum;
-// }
+    //   else{
+    //       document.getElementById('box').setAttribute("style", "border: 2px solid #43d32b !important;");
+    //       document.getElementById('box1').setAttribute("style", "color: #43d32b;");
+    //       console.log("el")
+    //   }
+    //   this.onum = this.nnum;
+    // }
   }
 };
-
-
-
-
-
-
-
-
-
 </script>
