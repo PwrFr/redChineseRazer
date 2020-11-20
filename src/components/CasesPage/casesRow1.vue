aae2925905b9cd02e88a989811c12de4b038589d
kaolnwza <kaow_jijuy999@hotmail.com>
2020-11-20 22:12:10 +0700

    lolza

---
+++ src/components/CasesPage/casesRow1.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="cases-all-row1">
+    <div class="cases-row1-header">BUILD IT. CHANGE IT. PERFECT IT.</div>
+    <div
+      class="cases-row1-des"
+    >To build the ultimate gaming desktop, it begins with a frame that can support the performance within. Meet the Razer Tomahawk ATX and Mini-ITX—gaming chassis that exude both form and function, equipped with premium features to accommodate your idea of the perfect rig, whatever it may be.</div>
+  </div>
+</template>
+<style >
+@font-face {
+  font-family: Roboto;
+  src: url("../assets/font/Roboto/Roboto-Thin.otf");
+}
+.cases-all-row1 {
+  text-align: center;
+  background-color: #44d62c;
+
+  height: 293px;
+  width: 100%;
+  width: 1930px;
+
+  margin: 0;
+
+  line-height: 1.5;
+  padding-top: 80px;
+}
+.cases-row1-header {
+  color: #000;
+  font-family: RazerF5, sans-serif;
+  font-size: 28px;
+  font-weight: 200;
+  grid-area: first;
+  letter-spacing: 0.0625rem;
+  line-height: 2.25rem;
+  padding-bottom: 13px;
+}
+.cases-row1-des {
+  font-family: RazerF5, sans-serif;
+  margin-left: 500px;
+
+  color: #000;
+  font-size: 20px;
+  font-weight: 200;
+  grid-area: second;
+  line-height: 1.5rem;
+  line-height: 1.75rem;
+  max-width: 930px;
+}
+</style>
