c56e313c5fb0f8a4af890caf586cfb594d55c6f5
Nutchayaporn <54405491+Nutchayaporn@users.noreply.github.com>
2020-11-23 01:33:35 +0700

    gaming

--- src/gamingLabtop.vue
+++ src/gamingLabtop.vue
@@ -9,22 +9,36 @@
       />
     </div>
   </div>
+  <div class="mt-4 mb-4">
+    <gamingBigBox
+      v-for="(i, index) in json.gaming_laptop_1"
+      :key="index"
+      :imgshow="json.gaming_laptop_1[index]"
+    />
+  </div>
+  <gamingBoxBottom
+    class="gaming-box-bottom"
+    v-for="(j, index) in json.gaming_laptop_2"
+    :key="index"
+    :info="json.gaming_laptop_2[index]"
+  />
 
-  gamingBigBox
+  <div></div>
 </template>
 
 <script>
-import datas from "../src/assets/data.json";      
+import datas from "../src/assets/data.json";
 import gamingtop from "./components/gaming-top.vue";
 import gamingSmallBox from "./components/gaming-small-box.vue";
-import gamingBigBox from "./components/gaming-big-box.vue"
+import gamingBigBox from "./components/gaming-big-box.vue";
+import gamingBoxBottom from "./components/gaming-box-bottom.vue";
 
 export default {
   name: "gamingLabtop",
   components: {
     gamingtop,
     gamingSmallBox,
-    gamingBigBox
+    gamingBigBox,
   },
   data() {
     return {
