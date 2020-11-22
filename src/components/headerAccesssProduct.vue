e9348058b24c70b6a13fd1195c74fe0149e69718
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-11-23 01:15:24 +0700

    AC1

--- src/components/headerAccesssProduct.vue
+++ src/components/headerAccesssProduct.vue
@@ -4,22 +4,24 @@
       class="razerf5 text-black pt-2 pb-0 container"
       style="font-size: 2.3rem"
     >
-      CARRY THE WINNING EDGE
+      {{ title }}
     </h1>
     <p
       class="container w-headerproduct razerf5 text-black pb-5 pt-0"
       style="font-size: 1.2rem"
     >
-      Weather any storm with the Razer Neoprene Sleeve V2 – For 13.3”
-      Notebooks—a tough, ultra-slim, and versatile sheath that keeps your Razer
-      Blade Stealth 13, or notebooks up to 13.3” shielded from harm, no matter
-      what the game brings.
+      {{ dis }}
     </p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    title: String,
+    dis: String,
+  },
+};
 </script>
 
 <style></style>
