ea8280e21cd6b302aff7b9c48aa537ad9489c9b1
Artermis <nopphadon.phanwong@gmail.com>
2020-11-23 13:22:31 +0700

    add detail monitorRow1

--- src/components/monitor/monitorRow1.vue
+++ src/components/monitor/monitorRow1.vue
@@ -8,8 +8,27 @@
             src="https://assets2.razerzone.com/images/pnx.assets/fac6ba0c209bdfc6ab05e9a74b3e9e43/kraken-te-toms-guide-award.png"
             alt=""
           />
+          <p class="pt-3">
+            <b>
+              “Razer's Raptor 27 gaming monitor is, quite <br />simply, a 144Hz
+              HDR triumph.”
+            </b>
+          </p>
+          <p class="gray-text-monitor">– PC Magazine</p>
         </div>
-        <div class="col-lg-6"></div>
+        <div class="col-lg-6 pt-3">
+          <img
+            src="https://assets2.razerzone.com/images/pnx.assets/fac6ba0c209bdfc6ab05e9a74b3e9e43/kraken-te-pcmag-award.png"
+            alt=""
+          />
+          <p class="pt-3">
+            <b>
+              “The Razer Raptor 27 delivers high style and <br />
+              performance and premium build quality. ”
+            </b>
+          </p>
+          <p class="gray-text-monitor">– Tom's Hardware</p>
+        </div>
       </div>
     </div>
   </div>
@@ -20,6 +39,9 @@
   height: 22.5625rem;
   font-family: RazerF5, Arial, Helvetica, sans-serif;
   text-align: center;
+}
+.gray-text-monitor {
+  color: rgb(136, 136, 136);
 }
 </style>
 <script>
