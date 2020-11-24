fe4e9374847f460d52097e4998898b2a696d329c
Artermis <nopphadon.phanwong@gmail.com>
2020-11-24 10:28:30 +0700

    update monitorRow3

--- src/components/monitor/monitorRow3.vue
+++ src/components/monitor/monitorRow3.vue
@@ -1,17 +1,59 @@
 <template>
   <div class="container-fluid" id="bg-monitorRow3">
-    <h2 id="header-monitorRow3">BIG, BRIGHT, BRILLIANT DCI-P3 COLOR</h2>
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 id="header-monitorRow3">BIG, BRIGHT, BRILLIANT DCI-P3 COLOR</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <p id="messageMonitorRow3">
+          One of the hallmarks of a premium display is the ability to show the
+          whole spectrum of vibrant colors, while also <br />rendering deep rich
+          blacks. The Razer Raptor does just that, featuring an IPS display
+          supporting HDR400 with an <br />insanely wide factory calibrated
+          DCI-P3 color gamut, which is quickly becoming the new film industry
+          standard and <br />surpasses the sRGB standard. With life-like colors
+          and WQHD resolution, the world will fade away as you are
+          <br />immersed in your favorite games and movies.
+        </p>
+      </div>
+    </div>
+    <div class="row" id="displayMonitorRow3">
+      <div class="col-lg-3">
+        <div class="row">
+          <h4>27”</h4>
+          <h5>Display</h5>
+        </div>
+      </div>
+      <div class="col-lg-3"></div>
+      <div class="col-lg-3"></div>
+    </div>
   </div>
 </template>
 <style>
 #bg-monitorRow3 {
   background-image: url("https://assets2.razerzone.com/images/pnx.assets/8311f08d80fc12fde210165c63334271/raptor-update-dev_04-usp1.jpg");
   width: 90rem;
-  height: 58.35rem;
+  min-height: 58.35rem;
+  background-position-x: center;
   justify-content: center;
+  font-family: RazerF5, Arial, Helvetica, sans-serif;
 }
 #header-monitorRow3 {
   color: green;
+  text-align: center;
+  padding: 2rem;
+}
+#messageMonitorRow3 {
+  color: rgb(136, 136, 136);
+  font-size: 1.2rem;
+  text-align: center;
+}
+#displayMonitorRow3 {
+  color: #fff;
+  justify-content: center;
+  justify-items: center;
 }
 </style>
 <script>
