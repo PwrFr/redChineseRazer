83694518f23645eea6fd02f25ce9ec8cdae8945f
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-11-25 00:33:24 +0700

    wtf

--- src/AppTestSlot.vue
+++ src/AppTestSlot.vue
@@ -1,29 +1,5 @@
 <template>
   <MenuBar />
-
-  <ProductSlot
-    class="mt-5"
-    imageAmount="5"
-    imageTab1="/src/assets/products/cases/bar/1.jpg"
-    imageTab2="/src/assets/products/cases/bar/2.jpg"
-    imageTab3="/src/assets/products/cases/bar/3.jpg"
-    imageTab4="/src/assets/products/cases/bar/4.jpg"
-    imageTab5="/src/assets/products/cases/bar/5.jpg"
-    backgroundSrc="/src/assets/products/cases/1.jpg"
-    imageLocateSrc="/src/assets/products/cases/"
-    :headerText="test"
-    headerDes="Mini-ITX Gaming Chassis with Razer Chroma RGB"
-    priceText="179.99"
-    desAmount="3"
-    description1="Dual-sided tempered glass swivel doors"
-    description2="Powered by Razer Chroma™ RGB"
-    description3="Ventilated top panel and dust filters"
-    modelBoxDisappear="0"
-    modelFirstItem="Tomahawk ATX"
-    shippingDate="Ships on November 05, 2020"
-    buyingStatus="ADD TO CART"
-    buyingAt="Also Available At"
-  />
 </template>
 
 <script>
@@ -35,15 +11,15 @@ export default {
   name: "AppTestSlot",
   components: {
     ProductSlot,
-    MenuBar
+    MenuBar,
   },
   data() {
     return {
-      test: datas.monitors[0].name
+      test: datas.monitors[0].name,
     };
   },
   created() {
     console.log(datas.monitors[0].name);
-  }
+  },
 };
 </script>
